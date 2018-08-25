365b1d2f8ab018257e3cf737d730d2e69920f86e
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2018-08-25 20:48:09 +0700

    Update profile page

--- resources/views/panel/profile.blade.php
+++ resources/views/panel/profile.blade.php
@@ -1,22 +1,23 @@
 @extends('master')
 @section('content')
 <div class="container-fluid">
-	<div class="animate fadeIn">
-        <h3 class="mb-5">Hotel Name Here</h3>
-        <div>
-            <div class="mr-3 btn btn-secondary">
-                <i class="fa fa-lightbulb-o"></i>&nbsp;
-                Edit Profile
-            </div>
-            <div class="mr-3 btn btn-secondary">
-                <i class="fa fa-lightbulb-o"></i>&nbsp;
-                Photo Shoots
-            </div>
-            <div class="mr-3 btn btn-secondary">
-                <i class="fa fa-lightbulb-o"></i>&nbsp;
-                Edit Photos
-            </div>
+    <h3 class="mb-5">Hotel Name Here</h3>
+    <div class="mb-4">
+        <div class="mr-3 btn btn-secondary">
+            <i class="fa fa-lightbulb-o"></i>&nbsp;
+            Edit Profile
         </div>
+        <div class="mr-3 btn btn-secondary">
+            <i class="fa fa-lightbulb-o"></i>&nbsp;
+            Photo Shoots
+        </div>
+        <div class="mr-3 btn btn-secondary">
+            <i class="fa fa-lightbulb-o"></i>&nbsp;
+            Edit Photos
+        </div>
+    </div>
+    <div class="border border-dark p-4">
+        profile
     </div>
 </div>
 @endsection
